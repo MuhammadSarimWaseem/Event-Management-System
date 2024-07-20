fa6b88526fc5a4bc8d1303d1ca72fdc3e3167089
muhammadsarimwaseem <sarimwaseem84@gmail.com>
2024-07-21 00:34:34 +0500

    updated

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const path = require('path')
+const userModel = require('./models/user')
+
+const app = express()
+const PORT = 8000
+
+app.set('view engine', 'ejs');
+app.set("views", path.resolve('./Views'))
+
+// Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+//routes
+
+app.get('/', (req, res) => {
+    return res.render('home')
+})
+
+app.get('/event', async (req, res) => {
+    let allUsers = await userModel.find()
+    return res.render('event', { users: allUsers })
+})
+
+app.get('/delete/:id', async (req, res) => {
+
+    let deleteuser = await userModel.findOneAndDelete({ _id: req.params.id })
+    return res.redirect('/event')
+})
+
+app.get('/edit/:id', async (req, res) => {
+    let editUser = await userModel.findOne({ _id: req.params.id })
+    return res.render('edit', { editUser })
+})
+
+app.post('/update/:id', async (req, res) => {
+    let { name, image, detail, date } = req.body
+    let editUser = await userModel.findOneAndUpdate({ _id: req.params.id }, { name, image, date, detail }, { new: true })
+    return res.redirect('/event')
+})
+
+app.post('/create', async (req, res) => {
+    let { name, image, date, detail } = req.body
+
+    let createdEvent = await userModel.create({
+        name, image, date, detail
+
+    })
+    res.redirect('/event')
+})
+
+app.listen(PORT, () => { console.log("server connected", PORT) })
---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+
+mongoose.connect(process.env.DATABASE_URL)
+    .then(() => { console.log("mongo connected") })
+    .catch(() => { console.log("Connection error") })
+    console.log("Database URL:", process.env.DATABASE_URL);
+
+//schema
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    detail: {
+        type: String,
+        required: true
+    }
+})
+
+
+// model
+const userModel = new mongoose.model("user", userSchema)
+
+
+module.exports = userModel
