4488ef9e742056d5ac85aae80e86fa25fe182dc0
muhammadsarimwaseem <sarimwaseem84@gmail.com>
2024-08-17 18:19:19 +0500

    updated

--- index.js
+++ index.js
@@ -1,57 +1,66 @@
-const express = require('express')
-const path = require('path')
-const userModel = require('./models/user')
-const dotenv = require("dotenv")
+const express = require('express');
+const path = require('path');
+const userModel = require('./models/user');
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+
 dotenv.config();
 
-const app = express()
+const app = express();
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 app.set('view engine', 'ejs');
-app.set("views", path.resolve('./views'))
+app.set("views", path.resolve('./views'));
 
 // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//routes
-
+// Routes
 app.get('/', (req, res) => {
-    return res.render('home')
-})
+    return res.render('home');
+});
 
 app.get('/event', async (req, res) => {
-    let allUsers = await userModel.find()
-    return res.render('event', { users: allUsers })
-})
+    let allUsers = await userModel.find();
+    return res.render('event', { users: allUsers });
+});
 
 app.get('/delete/:id', async (req, res) => {
-
-    let deleteuser = await userModel.findOneAndDelete({ _id: req.params.id })
-    return res.redirect('/event')
-})
+    await userModel.findOneAndDelete({ _id: req.params.id });
+    return res.redirect('/event');
+});
 
 app.get('/edit/:id', async (req, res) => {
-    let editUser = await userModel.findOne({ _id: req.params.id })
-    return res.render('edit', { editUser })
-})
+    let editUser = await userModel.findOne({ _id: req.params.id });
+    return res.render('edit', { editUser });
+});
 
 app.post('/update/:id', async (req, res) => {
-    let { name, image, detail, date } = req.body
-    let editUser = await userModel.findOneAndUpdate({ _id: req.params.id }, { name, image, date, detail }, { new: true })
-    return res.redirect('/event')
-})
+    let { name, image, detail, date } = req.body;
+    await userModel.findOneAndUpdate({ _id: req.params.id }, { name, image, date, detail }, { new: true });
+    return res.redirect('/event');
+});
 
 app.post('/create', async (req, res) => {
-    let { name, image, date, detail } = req.body
+    let { name, image, date, detail } = req.body;
+    await userModel.create({ name, image, date, detail });
+    res.redirect('/event');
+});
 
-    let createdEvent = await userModel.create({
-        name, image, date, detail
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
-    })
-    res.redirect('/event')
-})
+app.listen(process.env.PORT, () => { console.log("server connected", process.env.PORT) });
 
-app.listen(process.env.PORT, () => { console.log("server connected", process.env.PORT) })
-
-module.exports=app
+module.exports = app;
