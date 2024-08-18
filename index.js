3148cbcad13197b142cd1b2a5a0396c65684bf32
muhammadsarimwaseem <sarimwaseem84@gmail.com>
2024-08-18 14:40:06 +0500

    updated

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.post('/create', async (req, res) => {
 });
 
 // Error Handling Middleware
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
